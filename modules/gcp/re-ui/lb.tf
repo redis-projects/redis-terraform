ee2caa07333a664e7da503ebe4b526d9009593bd
deji alaran <deji.alaran@redislabs.com>
2021-05-29 13:44:47 +0000

    updates

--- modules/gcp/re-ui/lb.tf
+++ modules/gcp/re-ui/lb.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
 # compute pool that points at the proxy
 
 resource "google_compute_target_pool" "default" {
@@ -29,4 +37,12 @@ resource "google_compute_forwarding_rule" "tls" {
   port_range = "8443"
   ip_address = google_compute_address.default.address 
   target = google_compute_target_pool.default.id
+}
+
+resource "google_dns_record_set" "a" {
+  name = "${var.name}.ps-redislabs.com."
+  type = "A"
+  ttl = 60
+  managed_zone = "ps-redislabs" 
+  rrdatas = [ google_compute_address.default.address ]
 }
