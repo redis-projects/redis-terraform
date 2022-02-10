66cfc805558bb4a642c32ff852c7c28fb8123ce0
Deji <deji.alaran@redis.com>
2022-02-10 16:48:09 +0000

    script to stagger riot instances

---
+++ modules/docker/services/vi/stagger_riot_instances.sh
@@ -0,0 +1,17 @@
+FILE_LEN=$(grep -c '^' $1 | awk '{print $1}')
+while IFS="" read -r p || [ -n "$p" ]
+do
+    echo "$p"
+    let "COUNT=COUNT+1"
+    let "LINE_NO=LINE_NO+1"
+
+    if [ "$COUNT" -ge "$BATCH_SIZE" ] 
+    then     
+        if [ "$LINE_NO" -lt "$FILE_LEN" ] 
+        then
+            echo "sleeping for $SLEEP seconds"
+            sleep $SLEEP
+        fi
+        let "COUNT=0"
+    fi
+done < $1
