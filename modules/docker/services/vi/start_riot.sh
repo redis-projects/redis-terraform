7b1fc1f69e9afe80b8ff970549c3109f03ad981a
Deji <deji.alaran@redis.com>
2022-02-14 23:19:56 +0000

    update start riot script

--- modules/docker/services/vi/start_riot.sh
+++ modules/docker/services/vi/start_riot.sh
@@ -1,4 +1,4 @@
 docker run -d --rm \
  --name "riot-$1" \
  --net br0 riot \
-/home/gradle/riot-redis-2.14.6/bin/riot-redis -h ${SOURCE_URL} -p ${SOURCE_PORT} replicate ${TYPE_DS} -h ${DESTINATION_URL} -p ${DESTINATION_PORT} --batch 10000 --event-queue 100000 --mode live --threads ${NUM_THREADS}
+/home/gradle/riot-redis-2.14.6/bin/riot-redis -h ${SOURCE_URL} -p ${SOURCE_PORT} ${SOURCE_PASSWORD} replicate ${TYPE_DS} -h ${DESTINATION_URL} -p ${DESTINATION_PORT} ${DESTINATION_PASSWORD} --event-queue 100000 --mode live
